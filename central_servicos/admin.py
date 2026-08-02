586e572c7bcec829c5c0cc8abb2aba086998b5d6
Geziel Sena Colares <geziel.colares@gmail.com>
2026-08-02 13:35:18 -0400

    Atualiza configurações do VS Code e adiciona ignores do pyrefly em central_servicos/admin.py

--- central_servicos/admin.py
+++ central_servicos/admin.py
@@ -1,3 +1,4 @@
+# pyrefly: ignore [untyped-import]
 from django.contrib import admin
 from .models import Predio, Andar, Sala, AtivoPredial, OrdemServico, MaterialUtilizado, CategoriaServico
 
@@ -7,33 +8,40 @@ class MaterialUtilizadoInline(admin.TabularInline):
 
 @admin.register(Predio)
 class PredioAdmin(admin.ModelAdmin):
+    # pyrefly: ignore [bad-override-mutable-attribute]
     list_display = ('nome', 'sigla')
 
 @admin.register(Andar)
 class AndarAdmin(admin.ModelAdmin):
+    # pyrefly: ignore [bad-override-mutable-attribute]
     list_display = ('nome', 'predio')
     list_filter = ('predio',)
 
 @admin.register(Sala)
 class SalaAdmin(admin.ModelAdmin):
+    # pyrefly: ignore [bad-override-mutable-attribute]
     list_display = ('nome', 'andar')
     list_filter = ('andar__predio', 'andar')
 
 @admin.register(AtivoPredial)
 class AtivoPredialAdmin(admin.ModelAdmin):
+    # pyrefly: ignore [bad-override-mutable-attribute]
     list_display = ('nome', 'patrimonio', 'sala')
     list_filter = ('sala__andar__predio',)
     search_fields = ('nome', 'patrimonio')
 
 @admin.register(CategoriaServico)
 class CategoriaServicoAdmin(admin.ModelAdmin):
+    # pyrefly: ignore [bad-override-mutable-attribute]
     list_display = ('nome',)
     search_fields = ('nome',)
 
 @admin.register(OrdemServico)
 class OrdemServicoAdmin(admin.ModelAdmin):
+    # pyrefly: ignore [bad-override-mutable-attribute]
     list_display = ('numero', 'categoria', 'solicitante', 'status', 'prioridade', 'ativo_predial', 'sala', 'data_abertura')
     list_filter = ('status', 'categoria', 'prioridade', 'data_abertura')
     search_fields = ('numero', 'descricao_problema')
+    # pyrefly: ignore [bad-override-mutable-attribute]
     inlines = [MaterialUtilizadoInline]
     readonly_fields = ('numero', 'data_abertura', 'estoque_baixado')
