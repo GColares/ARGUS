4825e0e2ac2982dbb30bf1b0e5e52bcf9fe4b816
Geziel Sena Colares <geziel.colares@ifam.edu.br>
2026-08-31 18:15:41 -0400

    fix: obriga preenchimento de sigla e nome_fantasia em todas as filhas de PessoaJuridica

--- cadastros/models.py
+++ cadastros/models.py
@@ -79,7 +79,7 @@ class ICT(PessoaJuridica):
 
 class EmpresaParceira(PessoaJuridica):
     """Empresas de Base Tecnológica ou Indústrias (Concedentes)"""
-    nome_fantasia = models.CharField(max_length=255, verbose_name="Nome Fantasia", blank=True, null=True)
+    nome_fantasia = models.CharField(max_length=255, verbose_name="Nome Fantasia")
     PORTE_CHOICES = [
         ('ME', 'Microempresa'),
         ('EPP', 'Empresa de Pequeno Porte'),
@@ -94,7 +94,7 @@ class EmpresaParceira(PessoaJuridica):
 
 class FundacaoApoio(PessoaJuridica):
     """Fundações de Apoio (ex: FAEPI) - Gestão Financeira"""
-    sigla = models.CharField(max_length=20, verbose_name="Sigla da Fundação", blank=True, null=True)
+    sigla = models.CharField(max_length=20, verbose_name="Sigla da Fundação")
     registro_mec = models.CharField(max_length=100, verbose_name="Registro de Credenciamento MEC/MCTI", blank=True, null=True)
     validade_credenciamento = models.DateField(verbose_name="Validade do Credenciamento", blank=True, null=True)
 
@@ -276,7 +276,7 @@ class PlanoDeTrabalho(models.Model):
 
 class Fornecedor(PessoaJuridica):
     """Cadastro de Credores e Empresas fornecedoras com dados estendidos."""
-    sigla = models.CharField(max_length=50, blank=True, null=True, verbose_name="Sigla / Nome Curto")
+    sigla = models.CharField(max_length=50, verbose_name="Sigla / Nome Curto")
     email = models.EmailField(blank=True, null=True, verbose_name="E-mail de Contato")
 
     class Meta: # type: ignore
